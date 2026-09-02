dbc1c3149c70981c2e3b52d2a26627674b05ae6e
Ivan Caldarella <gamein3x@gmail.com>
2026-09-02 20:54:30 +0200

    first commit - primi 3 snack

---
+++ index.js
@@ -0,0 +1,42 @@
+// Crea una funzione che somma due numeri
+
+/// dichiarativa:
+
+    function somma(num1, num2) {
+        return num1 + num2
+    };
+
+console.log(somma(5, 3));
+
+/// anonima:
+
+    const somm = function(num1, num2) {return num1 + num2};
+
+    const sommEs = somm(1, 2);
+
+    console.log(sommEs);
+
+/// arrow:
+
+    const soamma = (a, b) => {return a + b};
+
+    const soammaEs = soamma(5, 4);
+
+    console.log(soammaEs);
+
+// Crea una funzione che fa il quadrato di un numero
+
+    const quadrato = n => n ** 2;
+
+    console.log(quadrato(2));
+
+// Crea una funzione "esegui operazione" con callback
+
+    function esegui(n1, n2, operatore) {
+        return somma(n1, n2);
+    }
+
+console.log(esegui(1, 88, somma));
+
+// Crea un generatore di funzioni "crea timer"
+
